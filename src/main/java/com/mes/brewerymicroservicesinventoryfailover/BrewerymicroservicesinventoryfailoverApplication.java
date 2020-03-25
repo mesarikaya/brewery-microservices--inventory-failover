d91e98969b43915121c8a23a86780c98489254e8
mesarikaya <mesarikaya@gmail.com>
2020-03-25 14:03:18 +0100

    First commit

---
+++ src/main/java/com/mes/brewerymicroservicesinventoryfailover/BrewerymicroservicesinventoryfailoverApplication.java
@@ -0,0 +1,13 @@
+package com.mes.brewerymicroservicesinventoryfailover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewerymicroservicesinventoryfailoverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrewerymicroservicesinventoryfailoverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mes/brewerymicroservicesinventoryfailover/BrewerymicroservicesinventoryfailoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mes.brewerymicroservicesinventoryfailover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewerymicroservicesinventoryfailoverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
